a060c29dbad0885da2676d70ec5569e7058aa367
robbiemallett <ucfarm0@login13.myriad.ucl.ac.uk>
2021-01-05 12:53:58 +0000

    filling vals for search.sh

--- search_mode_script.sh
+++ search_mode_script.sh
@@ -1,23 +1,23 @@
 #!/bin/bash -l
 
 # Request minutes of wallclock time (format hours:minutes:seconds).
-#$ -l h_rt=0:15:0
+#$ -l h_rt=12:00:0
 
 # Request gigabytes of RAM for each core/thread (must be an integer followed by M, G, or T)
 #$ -l mem=1G
 
 # Request gigabytes of TMPDIR space (default is 10 GB)
-#$ -l tmpfs=5G
+#$ -l tmpfs=2G
 
 # Set the name of the job.
-#$ -N testing
+#$ -N SMRT_fitting
 
 # Request cores.
 #$ -pe smp 1
 
 # Set up the job array.  In this instance we have requested 10000 tasks
 # numbered 1 to 10000.
-#$ -t 1-3
+#$ -t 1-20
 
 # Set the working directory to somewhere in your scratch space.
 #$ -wd /home/ucfarm0/Scratch
@@ -27,4 +27,5 @@
 #                                                     job_name      niter   hpc_flag
 
 source /home/ucfarm0/inverse_smrt/bin/activate
-python /home/ucfarm0/inverse_smrt/inverter/search_mode.py testing_$SGE_TASK_ID 2 -hpc
+python /home/ucfarm0/inverse_smrt/inverter/search_mode.py jan_2021_$SGE_TASK_ID 
+2 -hpc
