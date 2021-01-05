b7a46e98410117c47e84dcb1a6611bc04085bd9a
robbiemallett <robbiemallett@gmail.com>
2021-01-05 12:50:27 +0000

    updating job script

--- search_mode_script.sh
+++ search_mode_script.sh
@@ -27,5 +27,4 @@
 #                                                     job_name      niter   hpc_flag
 
 source /home/ucfarm0/inverse_smrt/bin/activate
-python /home/ucfarm0/inverse_smrt/inverter/main.py testing_$SGE_TASK_ID 2 -hpc
-
+python /home/ucfarm0/inverse_smrt/inverter/search_mode.py testing_$SGE_TASK_ID 2 -hpc
